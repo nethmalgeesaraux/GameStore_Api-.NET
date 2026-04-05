c47caf1428abdc405128594cfc73f3f60b91a236
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-05 22:38:34 +0530

    initialize API project with launch settings and HTTP request file

--- obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c28b9825349d39a4a77eec65018c9da35c83e7f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+113321fae772940f565fbd4405d41cd6703fc829")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
