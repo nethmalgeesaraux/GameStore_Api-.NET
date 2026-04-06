8017ced1cb8acc05849594eb6c0709c6aa849627
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-06 19:38:56 +0530

    Refactor GameDto to use record syntax and add sample game data in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,31 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+List<GameDto> games = [
+    new (
+        1,
+        "The Legend of Zelda: Breath of the Wild",
+        "Action-Adventure",
+        59.99m,
+        new DateOnly(2017, 3, 3)),
+
+    new (
+        2,
+        "God of War",
+        "Action-Adventure",
+        49.99m,
+        new DateOnly(2018, 4, 20)),
+
+    new (
+        3,
+        "Red Dead Redemption 2",
+        "Action-Adventure",
+        59.99m,
+        new DateOnly(2018, 10, 26)),
+
+];
 
 //Get /game
 app.MapGet("/game", () => "Hello World!");
