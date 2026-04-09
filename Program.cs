eee45f878020b73f299f552c46f6dafaa77ea5be
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-09 19:50:31 +0530

    Data seeding

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.Endpoints;
+using GameStore.Api.Models;
+using Microsoft.Extensions.Options;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +11,25 @@ builder.Services.AddValidation();
 
 var connString =  "Data Source= GameStore.db";
 
-builder.Services.AddSqlite<GameStoreContext>(connString);
+builder.Services.AddSqlite<GameStoreContext>(
+    connString,
+    optionsAction : Options => Options.UseSeeding((context, _) =>
+    {
+        if(!context.Set<GameModel>().Any())
+        {
+            context.Set<GameModel>().AddRange(
+                new GameModel { Name = "The Legend of Zelda: Breath of the Wild" },
+                new GameModel { Name = "Super Mario Odyssey" },
+                new GameModel { Name = "Red Dead Redemption 2" }
+            );
+
+            context.SaveChanges();
+           
+        }
+        
+    })
+    
+    );
 
 var app = builder.Build();
 
--- obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502e07d8daed8214b654e6ffdf892b4c116775c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63ad9621553ed6b39aa0b1e6398e4878cc099343")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
